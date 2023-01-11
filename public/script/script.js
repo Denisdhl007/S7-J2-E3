55ae29537ef210d9052b96fe28a7db55705029e9
Denisdhl007 <denis.henin@icloud.com>
2023-01-11 16:25:39 +0100

    1

--- public/script/script.js
+++ public/script/script.js
@@ -266,7 +266,7 @@
 // // ### Exercice 12
 // // - Une classe a 10 etudiants, 12 etudiants rentrent en classe mais les 2 derniers se sont trompés. Vous devez les envoyer dans une autre classe et montrer les etudiants qui se sont trompés et aussi montrer la classe complete avec les 10 autres etudiants.
 
-// test
+// // test
 let tableau = [];
 for(let i = 0; i < 10; i++) {
     tableau.push(Math.floor(Math.random() * 10));
